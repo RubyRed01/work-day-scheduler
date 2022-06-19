9b861bb358d27f402ed2201571c21389c7648144
RubyRed01 <104714810+RubyRed01@users.noreply.github.com>
2022-06-19 10:53:16 +1000

    Save button created and coded

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ event.preventDefault()
 var timeBlock = $(this).attr("id").split("-")[0]
 var userEntry = $("#"+timeBlock+"-txt").val()
 console.log(timeBlock,userEntry)
+localStorage.setItem(timeBlock, userEntry)
+
 })
+
+for(let i=9;i<=17;i++){
+    var userPlan = localStorage.getItem(i)
+    $("#"+i+"-txt").val(userPlan)
+}
