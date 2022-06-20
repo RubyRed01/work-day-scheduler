5fad11a0ba5c73f7c90a40b8b68ee3a434ea0f00
RubyRed01 <104714810+RubyRed01@users.noreply.github.com>
2022-06-20 23:15:03 +1000

    Commit ready for assignment submission

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ console.log(timeBlock,userEntry)
 localStorage.setItem(timeBlock, userEntry)
 
 })
+var currentDay = moment().format('ll'); 
+$("#currentDay").text(currentDay)
+var currentHour = moment().hours()
 
 for(let i=9;i<=17;i++){
     var userPlan = localStorage.getItem(i)
     $("#"+i+"-txt").val(userPlan)
+    if(currentHour < i){
+        $("#"+i+"-txt").addClass("future")
+    }else if(currentHour===i){
+        $("#"+i+"-txt").addClass("present")
+    }else {
+        $("#"+i+"-txt").addClass("past")
+    }
+
 }
